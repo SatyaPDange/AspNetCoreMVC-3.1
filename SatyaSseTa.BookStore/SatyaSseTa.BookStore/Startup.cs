ef578214bdba1860b34490fa3ad9c7a4d03b8290
LENOVO <LENOVO@SUBODH48>
2020-10-11 11:29:07 +0530

    Added MVC to application

--- SatyaSseTa.BookStore/SatyaSseTa.BookStore/Startup.cs
+++ SatyaSseTa.BookStore/SatyaSseTa.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace SatyaSseTa.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,20 +31,8 @@ namespace SatyaSseTa.BookStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.Map("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.Map("/satya", async context =>
-                {
-                    await context.Response.WriteAsync("Hello Satya!");
-                });
-            });
-
         }
     }
 }
