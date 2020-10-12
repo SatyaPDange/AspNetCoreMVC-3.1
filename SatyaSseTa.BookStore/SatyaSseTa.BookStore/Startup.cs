90c04660088c9b01b967b7e292cf22600121d784
LENOVO <LENOVO@SUBODH48>
2020-10-12 15:00:48 +0530

    Added libman to use client side library

--- SatyaSseTa.BookStore/SatyaSseTa.BookStore/Startup.cs
+++ SatyaSseTa.BookStore/SatyaSseTa.BookStore/Startup.cs
@@ -31,11 +31,11 @@ namespace SatyaSseTa.BookStore
 
             app.UseStaticFiles();
 
-            app.UseStaticFiles(new StaticFileOptions()
-            { 
-            FileProvider= new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "MyStaticFiles")),
-            RequestPath = "/MyStaticFiles"
-            });
+            //app.UseStaticFiles(new StaticFileOptions()
+            //{ 
+            //FileProvider= new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "MyStaticFiles")),
+            //RequestPath = "/MyStaticFiles"
+            //});
 
 
 
